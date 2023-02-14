5f859c15c21e2bdff653173463745b9d8b8060de
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-02-14 10:46:33 -0600

    form ts con custom hooks

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -2,7 +2,10 @@ import { useForm } from '../hooks/useForm';
 
 export const Formularios = () => {
 
-    const {formulario, onChange} = useForm();
+    const {formulario, email, password, onChange} = useForm({
+        email: 'correo@correo.com',
+        password: '123456'
+    });//desestructuracion del formulario, email y password, asi como la funcion onChange
 
     return (
         <>
@@ -11,7 +14,7 @@ export const Formularios = () => {
                 type="text"
                 className="form-control"
                 placeholder="Email"
-                value={formulario.email}//valores por defecto del formulario desde el estado
+                value={email}//valores por defecto del formulario desde el estado
                 onChange={({ target }) => onChange(target.value, 'email')}//cambio de valores del formulario
             />
 
@@ -19,7 +22,7 @@ export const Formularios = () => {
                 type="text"
                 className="form-control mt-2 mb-2"
                 placeholder="Password"
-                value={formulario.password}
+                value={password}
                 onChange={({ target }) => onChange(target.value, 'password')}
             />
             {/* //imprime el formulario */}
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,20 +1,18 @@
 import { useState } from 'react';
 
-export const useForm = () => {
-    const [formulario, setFormulario] = useState({//manejo de estado para el formulario
-        email: 'correo@correo.com',
-        password: '123456'
-    });
+export const useForm = <T extends Object>( formulario: T ) => {
+    const [state, setState] = useState(formulario);//maneja el estado del formulario
 
-    const onChange = (value: string, campo: string) => {//funcion para manejar los cambios en el formulario que recibe el valor y el campo
-        setFormulario({
-            ...formulario,//copia del formulario
+    const onChange = (value: string, campo: keyof T) => {//funcion para manejar los cambios en el formulario que recibe el valor y el campo
+        setState({
+            ...state,//copia del formulario
             [campo]: value//cambio del campo
         })
     }
 
     return {
-        formulario,
+        ...state,//retorna el formulario
+        formulario: state,
         onChange
     }
 }
